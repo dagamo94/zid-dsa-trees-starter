dc2e08ebf3868580a02aeddc82a2058dcf3bfa73
dan <da.ga.mo.94@gmail.com>
2022-05-19 09:54:27 -0600

    BST and its methods implemented

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,3 +1,5 @@
+const Queue = require("./Queue");
+
 class BinarySearchTree {
   // your code here
   constructor(key = null, value = null, parent = null) {
@@ -25,6 +27,22 @@ class BinarySearchTree {
         this.right.insert(key, value);
       }
     }
+  }
+
+  getHeight(currentHeight = 0) {
+    if (!this.right && !this.left) {
+      return currentHeight;
+    }
+
+    const newHeight = currentHeight + 1;
+
+    if (!this.left) return this.right.getHeight(newHeight);
+    if (!this.right) return this.left.getHeight(newHeight);
+
+    const leftHeight = this.left.getHeight(newHeight);
+    const rightHeight = this.right.getHeight(newHeight);
+
+    return Math.max(leftHeight, rightHeight);
   }
 
   find(key) {
@@ -64,6 +82,27 @@ class BinarySearchTree {
     }
   }
 
+  isBST() {
+    const values = this.dfsInOrder();
+
+    for (let i = 1; i < values.length; ++i) {
+      if (values[i] < values[i - 1]) return false;
+    }
+
+    return true;
+  }
+
+  findKthLargestNumber(k){
+    const values = this.dfsInOrder();
+    const kthIndex = values.length - k;
+
+    if(kthIndex >= 0){
+      return values[kthIndex];
+    }else{
+      throw new Error("'k' value exceeds the size of the BST.");
+    }
+  }
+
   _replaceWith(node) {
     if (this.parent) {
       if (this == this.parent.left) {
@@ -90,12 +129,74 @@ class BinarySearchTree {
     }
   }
 
-  _findMin(){
-    if(!this.left){
+  _findMin() {
+    if (!this.left) {
       return this;
     }
 
     return this.left._findMin();
+  }
+
+  dfsInOrder(values = []) {
+    if (this.left) {
+      values = this.left.dfsInOrder(values);
+    }
+
+    values.push(this.value);
+
+    if (this.right) {
+      values = this.right.dfsInOrder(values);
+    }
+    return values;
+  }
+
+  dfsPreOrder(values = []) {
+    values.push(this.value);
+
+    if (this.left) {
+      values = this.left.dfsPreOrder(values);
+    }
+
+    if (this.right) {
+      values = this.right.dfsPreOrder(values);
+    }
+
+    return values;
+  }
+
+  dfsPostOrder(values = []) {
+    if (this.left) {
+      values = this.left.dfsPostOrder(values);
+    }
+
+    if (this.right) {
+      values = this.right.dfsPostOrder(values);
+    }
+
+    values.push(this.value);
+
+    return values;
+  }
+
+  bfs(tree, values = []) {
+    const queue = new Queue();
+    queue.enqueue(tree); // Start the traversal a t the tree and add the tree node to the queue to kick off the BFS
+    let node = queue.dequeue(); // Remove form the queue
+    while (node) {
+      values.push(node.value); // Add that dequeued value from the queue to an array
+
+      if (node.left) {
+        queue.enqueue(node.left); // Add the left child to the queue
+      }
+
+      if (node.right) {
+        queue.enqueue(node.right); // Add the right child to the queue
+      }
+
+      node = queue.dequeue();
+
+    }
+    return values;
   }
 }
 
